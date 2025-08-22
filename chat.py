037fc03978d86992753d1d26feb508f5c66b64dc
Keming <kz138@duke.edu>
2025-08-22 13:31:05 +0800

    correct liteLLM

--- chat.py
+++ chat.py
@@ -2,17 +2,27 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
+
 # Load environment variables from .env file
 load_dotenv()
 
-# Initialize the OpenAI client
+key = os.getenv("OPENAI_API_KEY")
+print("你的 key 是：", repr(key))
+print("长度：", len(key) if key else "没找到")
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("没有找到 OPENAI_API_KEY，请先在系统里设置环境变量。")
+
 client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY")  # Pull the API key from environment variables
-)
+    api_key=os.getenv("OPENAI_API_KEY"), ###你的_Duke_LiteLLM_API_KEY
+    base_url="https://litellm.oit.duke.edu"
+    )
 
 # Call the Chat Completion endpoint
 response = client.chat.completions.create(
-    model="gpt-4o-mini",
+    model="gpt-5",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user",   "content": "Hello! How are you today?"}
